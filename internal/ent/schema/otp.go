ce66c469ca619a95d7a94575fb6188cbfb2a3792
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-12-15 17:28:10 +0700

    feat(ent): add OTP & activation

---
+++ internal/ent/schema/otp.go
@@ -0,0 +1,69 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// OTP holds the schema definition for the OTP entity.
+type OTP struct {
+	ent.Schema
+}
+
+// Mixin for the OTP.
+func (OTP) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		BaseMixin{},
+	}
+}
+
+// Fields of the OTP.
+func (OTP) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("code").
+			MaxLen(6).
+			Sensitive().
+			Unique().
+			NotEmpty().
+			SchemaType(map[string]string{dialect.Postgres: "char(6)"}).
+			Comment("The OTP code must be 6 digits long. Will be deleted after it is used."),
+		field.Enum("type").NamedValues(
+			"ResetPassword", "RESET_PASSWORD",
+			"ChangePassword", "CHANGE_PASSWORD",
+		).StructTag(`json:"type"`),
+		field.Bool("is_used").Default(false).StructTag(`json:"is_used"`),
+		field.Int64("expired_at").
+			Positive().
+			Comment("The OTP code is only valid for 10 minutes.").
+			StructTag(`json:"expired_at"`),
+	}
+}
+
+// Edges of the OTP.
+func (OTP) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("account", Account.Type).
+			Ref("otp").
+			Required().
+			Unique(),
+	}
+}
+
+// Annotations of the OTP.
+func (OTP) Annotations() []schema.Annotation {
+	withComment := true
+
+	return []schema.Annotation{
+		&entsql.Annotation{
+			Table:        "otps",
+			WithComments: &withComment,
+			Checks: map[string]string{
+				"code": "length(code) = 6",
+			},
+		},
+	}
+}
