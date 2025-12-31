0aacc93b2a908105abda6038d0b1ad4bb9d38253
Ahmad Rifa'i <personal.rifai@gmail.com>
2026-01-01 00:34:14 +0700

    feat(redisx): add Redis connection

---
+++ internal/database/redisx/redisx.go
@@ -0,0 +1,67 @@
+package redisx
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/sembraniteam/setetes/internal"
+)
+
+type (
+	client struct {
+		config internal.Config
+	}
+
+	Redis interface {
+		Connect() *redis.Client
+		Disconnect(rdb *redis.Client)
+	}
+)
+
+func New() Redis {
+	return client{config: *internal.Get()}
+}
+
+func (c client) Connect() *redis.Client {
+	redisConf := c.config.Redis
+	cl := redis.NewClient(&redis.Options{
+		Addr:            fmt.Sprintf("%s:%d", redisConf.Host, redisConf.Port),
+		Username:        redisConf.Username,
+		Password:        redisConf.Password,
+		DB:              redisConf.Database,
+		MaxRetries:      redisConf.MaxRetries,
+		DialTimeout:     redisConf.DialTimeout,
+		ReadTimeout:     redisConf.ReadTimeout,
+		WriteTimeout:    redisConf.WriteTimeout,
+		PoolSize:        redisConf.PoolSize,
+		PoolTimeout:     redisConf.PoolTimeout,
+		MinIdleConns:    redisConf.MinIdleConns,
+		MaxIdleConns:    redisConf.MaxIdleConns,
+		MaxActiveConns:  redisConf.MaxActiveConns,
+		ConnMaxLifetime: redisConf.ConnMaxLifetime,
+		ConnMaxIdleTime: redisConf.ConnMinIdleTime,
+		TLSConfig: &tls.Config{
+			ServerName:         redisConf.Host,
+			MinVersion:         tls.VersionTLS12,
+			InsecureSkipVerify: true,
+			ClientAuth:         tls.RequireAndVerifyClientCert,
+		},
+	})
+
+	_, err := cl.Ping(context.Background()).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return cl
+}
+
+func (c client) Disconnect(rdb *redis.Client) {
+	defer func(rdb *redis.Client) {
+		if err := rdb.Close(); err != nil {
+			panic(err)
+		}
+	}(rdb)
+}
